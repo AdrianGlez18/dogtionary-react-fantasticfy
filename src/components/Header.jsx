a396f61397f924c3c39f9d06febc82356b1b57e0
AdrianGlez18 <adrianglezhdez18@gmail.com>
2022-07-07 19:55:16 +0100

    Minor image bug with Vite solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Input, Box, HStack, VStack } from '@chakra-ui/react'
 import ThemeButton from "./ThemeButton";
+import Logo from '../assets/images/logo.png'
 
 const Header = ({ handleSearchChange }) => {
     return (
         <div>
             <VStack alignContent={"center"} justifyContent="center" py={4}>
-                <img src="/src/assets/images/logo.png" height="100px" />
+                <img src={Logo} height="100px" />
                 <Box maxW="1280px">
                     <HStack spacing={5} align="center" p={4}>
                         <Input placeholder='Search a dog breed :)' size='md' onChange={event => handleSearchChange(event.target.value)} />
